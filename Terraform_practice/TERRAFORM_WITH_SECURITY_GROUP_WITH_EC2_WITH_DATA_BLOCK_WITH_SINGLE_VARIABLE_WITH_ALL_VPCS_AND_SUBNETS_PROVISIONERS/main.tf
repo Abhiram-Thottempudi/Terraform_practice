fcc1fcdf18e22df98870431198b11ed8514e4ebc
Abhiram Thottempudi <thottempudiabhiram@gmail.com>
2026-01-18 23:03:21 +0530

    Update main.tf

--- Terraform_practice/TERRAFORM_WITH_SECURITY_GROUP_WITH_EC2_WITH_DATA_BLOCK_WITH_SINGLE_VARIABLE_WITH_ALL_VPCS_AND_SUBNETS_PROVISIONERS/main.tf
+++ Terraform_practice/TERRAFORM_WITH_SECURITY_GROUP_WITH_EC2_WITH_DATA_BLOCK_WITH_SINGLE_VARIABLE_WITH_ALL_VPCS_AND_SUBNETS_PROVISIONERS/main.tf
@@ -1,38 +1,51 @@
 data "aws_vpc" "mynewvpc" {
-    default = true
+    filter {
+        name = "tag:Name"
+        values = ["mynewvpc"]
+    }
+}
+data "aws_subnet" "subnets_info" {
+    vpc_id = data.aws_vpc.mynewvpc.id
+    filter {
+        name = "tag:Name"
+        values = ["sub1"]
+    }
 }
 data "aws_security_group" "mynewsg" {
+    vpc_id = data.aws_vpc.mynewvpc.id
     filter {
-        name = "group-name"
+        name   = "group-name"
         values = ["mynewsg"]
-    }
+        }
 }
-data "aws_ami" "myami" {
+data "aws_ami" "mynewami" {
     most_recent = true
-    owners = ["099720109477"]
+    owners      = ["099720109477"]
     filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20251204"]
-    }
+        name   = "name"
+        values = ["ubuntu/images-testing/hvm-ssd-gp3/ubuntu-noble-daily-amd64-server-20250927"]
+        }
 }
+
 resource "aws_key_pair" "mynewkeypair" {
     public_key = file("~/.ssh/id_ed25519.pub")
-    key_name = "mysshkey"
+    key_name   = "myssh-key"
 }
 resource "aws_instance" "mynewec2" {
-    ami = data.aws_ami.myami.id
+    ami                         = data.aws_ami.mynewami.id
+    instance_type               = "t3.micro"
+    key_name                    = aws_key_pair.mynewkeypair.key_name
     associate_public_ip_address = true
-    key_name = aws_key_pair.mynewkeypair.key_name
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [ data.aws_security_group.mynewsg.id ]
+    subnet_id = data.aws_subnet.subnets_info.id
+    vpc_security_group_ids      = [data.aws_security_group.mynewsg.id]
     tags = {
         Name = "mynewec2"
-    }
+        }
     connection {
-        type = "ssh"
-        user = "ubuntu"
+        type        = "ssh"
+        user        = "ubuntu"
         private_key = file("~/.ssh/id_ed25519")
-        host = aws_instance.mynewec2.public_ip
+        host        = aws_instance.mynewec2.public_ip
     }
     provisioner "remote-exec" {
         inline = [ "sudo apt update",
